10ec826bd55b521b4cf0f7e54f577b8698834644
Cezar Azevedo <chep.chaf@gmail.com>
2015-10-20 18:18:19 -0300

    Now it's using gsub on nil...

--- world.rb
+++ world.rb
@@ -1,22 +1,22 @@
 load 'tiles.rb'
-@world = {}
+@world = Hash.new(0)
 @starting_position = [0, 0]
 
 def load_tiles
 	File.open('resources/map.txt', 'r') do |file|
       @rows = file.readlines
 	end
-	x_max = (@rows[0].split('\t')).length
-	@rows.each do |row, y|
-      @cols = row.split('\t')
+	x_max = (@rows[0].split(' ')).length
+	@rows.each_with_index do |row, y|
+      @cols = row.split(' ')
       x_max.times do |x|
-        tile_name = @cols[x].gsub("\r\n", '').chomp
+        tile_name = @cols[x].gsub("\r\n", '')
         if tile_name == 'StartingRoom'
-        	@starting_position = [x, y]
+          @starting_position = [x, y]
         end
         @world[x => y] = 
         if tile_name == ''
-           nil
+          nil
         else  
           eval "#{tile_name}.new(x, y)"
         end
