2624d9c5216731869ce346d534488cab414f04ed
Cezar Azevedo <chep.chaf@gmail.com>
2015-10-20 18:32:59 -0300

    It works, for now

--- world.rb
+++ world.rb
@@ -6,16 +6,16 @@ def load_tiles
 	File.open('resources/map.txt', 'r') do |file|
       @rows = file.readlines
 	end
-	x_max = (@rows[0].split('\t')).length
+	x_max = (@rows[0].split("\t")).length
 	@rows.each_with_index do |row, y|
-      @cols = @rows[y].split('\t')
+      @cols = @rows[y].split("\t")
       x_max.times do |x|
-        tile_name = @cols[x].gsub("\r\n", '') #unless @cols[x] == nil
+        tile_name = @cols[x].gsub("\n", '') unless @cols[x] == nil
         if tile_name == 'StartingRoom'
           @starting_position = [x, y]
         end
         @world[x => y] = 
-        if tile_name == '' #	|| tile_name == nil
+        if tile_name == '' || tile_name == nil
           nil
         else  
           eval "#{tile_name}.new(x, y)"
